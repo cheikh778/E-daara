192a6ffd3fdbb7cb26934921f421f0ca8e370bf4
Cheikh NDAO <cheikhndao@192.168.1.13>
2024-09-02 23:24:02 +0000

    dashboard

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    document.getElementById('navLinks').classList.toggle('active');
-});
-
-window.addEventListener('scroll', function() {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('navbar-scrolled');
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-    }
-});
-
-
-document.getElementById('toggleButton').addEventListener('click', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggleButton');
     var navLinks = document.getElementById('navLinks');
     var authButtons = document.getElementById('auth-buttons');
+    var navbar = document.getElementById('navbar');
 
-    // Toggle the 'active' class on navLinks and authButtons
-    navLinks.classList.toggle('active');
-    authButtons.classList.toggle('active');
+    
+    if (toggleButton && navLinks && authButtons) {
+        toggleButton.addEventListener('click', function() {
+            navLinks.classList.toggle('active');
+            authButtons.classList.toggle('active');
+        });
+    }
+
+    if (navbar) {
+        window.addEventListener('scroll', function() {
+            if (window.scrollY > 50) {
+                navbar.classList.add('navbar-scrolled');
+            } else {
+                navbar.classList.remove('navbar-scrolled');
+            }
+        });
+    }
+
+    // Active link logic
+    var currentPath = window.location.pathname;
+    var navLinkElements = document.querySelectorAll('.nav-link');
+
+    navLinkElements.forEach(function(link) {
+        if (link.getAttribute('href') === currentPath) {
+            link.classList.add('active');
+        }
+        link.addEventListener('click', function() {
+            // Remove active class from all links
+            navLinkElements.forEach(function(l) {
+                l.classList.remove('active');
+            });
+            // Add active class to the clicked link
+            link.classList.add('active');
+        });
+    });
 });
+
+
+
+// AOS Initialization
+AOS.init();
